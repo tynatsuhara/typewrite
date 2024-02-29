a818d6854dbc50405599fe3982e2f47ab1c5e576
Tyler Natsuhara <ty@ty.pizza>
2024-02-28 18:26:34 -0800

    hide toolbar until hover

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -6,10 +6,16 @@ import Save from '@suid/icons-material/Save'
 import SaveAs from '@suid/icons-material/SaveAs'
 // import Settings from '@suid/icons-material/Settings'
 import { Box, ButtonGroup, ThemeProvider, createTheme } from '@suid/material'
+import { createSignal } from 'solid-js'
 import { Doc } from '../core/Doc'
 import { FilingCabinet } from '../utils/FilingCabinet'
 import { createFullscreenSignal } from '../utils/Fullscreen'
+import { onEvent } from '../utils/onEvent'
 import { ToolbarButton } from './ToolbarButton'
+
+// TODO
+const TOOLBAR_HOVER_AREA = 150
+const TOOLBAR_OFFSCREEN_POS = '-230px'
 
 const theme = createTheme({
   palette: {
@@ -25,67 +31,67 @@ const theme = createTheme({
 // TODO: figure out colors
 export const UI = () => {
   const [isFullscreen, setFullscreen] = createFullscreenSignal()
+  const [isHoveringToolbarArea, setIsHoveringToolbarArea] = createSignal(false)
+
+  onEvent('mousemove', (e) => {
+    setIsHoveringToolbarArea(
+      e.clientY > document.body.clientHeight - TOOLBAR_HOVER_AREA &&
+        e.clientX < document.body.clientWidth / 4
+    )
+  })
 
   return (
     <ThemeProvider theme={theme}>
-      <Box
-      // sx={{
-      //   ':hover': {
-      //     opacity: '1',
-      //   },
-      //   opacity: 0.5,
-      // }}
+      <ButtonGroup
+        color="primary"
+        variant="contained"
+        sx={{
+          position: 'absolute',
+          left: isHoveringToolbarArea() ? '1rem' : TOOLBAR_OFFSCREEN_POS,
+          bottom: '1rem',
+          transition: 'left .3s ease',
+        }}
       >
-        <ButtonGroup
-          color="primary"
-          variant="contained"
-          sx={{
-            position: 'absolute',
-            left: '1rem',
-            bottom: '1rem',
+        <ToolbarButton onClick={() => FilingCabinet.put()}>
+          <Save />
+          {Doc.hasUnsavedChanges() && (
+            <Box
+              sx={{
+                position: 'absolute',
+                top: '1px',
+                right: '10px',
+              }}
+            >
+              *
+            </Box>
+          )}
+        </ToolbarButton>
+
+        <ToolbarButton>
+          <SaveAs />
+        </ToolbarButton>
+
+        <ToolbarButton onClick={() => FilingCabinet.get('TODOname')}>
+          <FileOpen />
+        </ToolbarButton>
+
+        <ToolbarButton>
+          <Print />
+        </ToolbarButton>
+
+        <ToolbarButton
+          onClick={() => {
+            setFullscreen(!isFullscreen())
+            blur()
           }}
         >
-          <ToolbarButton onClick={() => FilingCabinet.put()}>
-            <Save />
-            {Doc.hasUnsavedChanges() && (
-              <Box
-                sx={{
-                  position: 'absolute',
-                  top: '1px',
-                  right: '10px',
-                }}
-              >
-                *
-              </Box>
-            )}
-          </ToolbarButton>
+          {isFullscreen() ? <FullscreenExit /> : <Fullscreen />}
+        </ToolbarButton>
 
-          <ToolbarButton>
-            <SaveAs />
-          </ToolbarButton>
-
-          <ToolbarButton onClick={() => FilingCabinet.get('TODOname')}>
-            <FileOpen />
-          </ToolbarButton>
-
-          <ToolbarButton>
-            <Print />
-          </ToolbarButton>
-
-          <ToolbarButton
-            onClick={() => {
-              setFullscreen(!isFullscreen())
-              blur()
-            }}
-          >
-            {isFullscreen() ? <FullscreenExit /> : <Fullscreen />}
-          </ToolbarButton>
-
-          {/* <ToolbarButton>
+        {/* <ToolbarButton>
             <Settings />
           </ToolbarButton> */}
-        </ButtonGroup>
-      </Box>
+      </ButtonGroup>
     </ThemeProvider>
   )
 }
