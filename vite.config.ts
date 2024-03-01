e7afe9dca1327dadf6679d2dd88c5affece4266b
Tyler Natsuhara <ty@ty.pizza>
2024-02-29 17:59:55 -0800

    add print page

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
+  plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'print/index.html'),
+      },
+    },
   },
   // the project is deployed at ty.pizza/typewrite, so we need to configure the base path
   base: process.env.NODE_ENV === 'production' ? 'typewrite/' : '/',
